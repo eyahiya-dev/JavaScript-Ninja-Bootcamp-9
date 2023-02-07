f82b2123d1dc60a290b093c60396099e692a9d6a
eyahiya-dev <eyahiya.dev@gmail.com>
2023-02-07 17:21:25 +0600

    Practice Function in JS

---
+++ Day - 006/Exercise/README.md
@@ -0,0 +1,22 @@
+# Function related exercise and practice
+
+---
+
+#### 1.When and why function is needed?
+
+Ans: Function as like a machine in programming.Function make easy the code and reduce the repetition of the code.There are some reason for use Function.
+
+- Reusability
+- Abstraction
+
+Together, these two reasons make functions extremely useful--practically essential!-for programmers who write large programs. The ability to divide a program into abstract, reusable pieces is what makes it possible to write large programs that actually work right.
+
+#### 2.Write down and recap the difference between console and Return ?
+
+Ans: In the procedure of Function using return is a best practice.
+
+- Using Console.log :- When use console the returning value (result) only use and output inside the function and reuse returning value is hassle.
+  <br>
+- Using return:- This is the best practice and most beneficial for coding and time.Following it's reuse the returning value is so simple and effective.
+
+##### For Exercise Coding part please check ( exercise03.js ) file.
---
+++ Day - 006/Exercise/exercise03.js
@@ -0,0 +1,84 @@
+// 3.write this function takes in two parameters and returns the difference of the two;
+// difference(10,2);
+// difference(0,2);
+
+function difference(num, num2) {
+  return num - num2;
+}
+console.log(difference(10, 2));
+console.log(difference(0, 2));
+
+/*
+// 4. Write a function that takes a value as argument and return the type of value-
+
+myFunction(1) //'number'
+myFunction(false) //'boolean'
+myFunction({}) // 'object'
+myFunction(null) // 'object'
+myFunction('string') //'string'
+myFunction(['array']) //'object'
+*/
+
+function myFunction(str) {
+  return typeof str;
+}
+console.log(myFunction(1));
+console.log(myFunction(false));
+console.log(myFunction({}));
+console.log(myFunction(null));
+console.log(myFunction("string"));
+console.log(myFunction(["array"]));
+
+/*
+
+//5.Write Down this function takes in one parameter (a number from 1-7) and returns the day of the week (1 is saturday, 2 is Sunday, 3 is Monday etc.). If the number is less than 1 or greater than 7, the function should return undefined; Hint: you can use array to track down day of the week:)
+
+printDay(4); // "Tuesday"
+printDay(7); // "Friday"
+printDay(41); // undefined
+
+*/
+
+function printDay() {
+  const arr = [
+    "Saterday",
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+  ];
+  return arr;
+}
+console.log(printDay()[2]);
+console.log(printDay()[3]);
+console.log(printDay()[5]);
+console.log(printDay()[41]);
+
+/*
+
+// 6.Write down this function takes in one parameter (an array) and returns the last value in the array
+It should return undefined if the array is empty.
+
+N.B: count last element index dynamically :)
+
+lastElement([1,2,3,4]); // 4
+lastElement([1,2,3,10]); // 10
+lastElement([1,2,3,'hi']); // 'hi'
+lastElement([]); // undefined
+
+*/
+
+// Facing problem
+
+/*
+// 7. write a function that return a object based on passed arguments. Returned object key must be x , y, z in order and value will be passed arguments order- 
+
+createObj(1, 2, 3) //{x: 1, y: 2, z:3}
+createObj('a','b', 'c') //{x: 'a', y: 'b', z:'c'}
+createObj('ab','bc', 'ca') //{x: 'ab', y: 'bc', z:'ca'}
+
+*/
+
+// facing problem
