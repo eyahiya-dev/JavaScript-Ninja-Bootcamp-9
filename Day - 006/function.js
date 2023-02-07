7c7c14562282c167bc7cdf3545e1ebe11e7e3831
eyahiya-dev <eyahiya.dev@gmail.com>
2023-02-07 13:09:28 +0600

    practice function

---
+++ Day - 006/function.js
@@ -0,0 +1,9 @@
+function printProfile() {
+  console.log("Hello bangladesh");
+  console.log("Hello bangladesh");
+  console.log("Hello bangladesh");
+  console.log("Hello bangladesh");
+  console.log("Hello bangladesh");
+}
+
+printProfile();
