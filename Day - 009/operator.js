680258645dedcb18426d0844f75d48f37e692ba2
eyahiya-dev <eyahiya.dev@gmail.com>
2023-02-10 03:39:24 +0600

    Practice Opearator in Js 2

---
+++ Day - 009/operator.js
@@ -0,0 +1,28 @@
+/*
+# Falsy value
+- false
+- 0
+- null
+- undefined
+- NaN
+- '' empty string
+*/
+
+// Checkign Truthy or Falsy value
+
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean(false));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+console.log(Boolean(undefined));
+
+console.log(Boolean('eyahiya')  === true );
+console.log(Boolean(0) === true);
+console.log(!"");
+console.log(!"Eyahiya");
+console.log(!!"Eyahiya");
+
+// 
+
+
