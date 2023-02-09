c10540c04221fd4ecfb67e10837eec1df868ee79
eyahiya-dev <eyahiya.dev@gmail.com>
2023-02-10 04:11:04 +0600

    Practice Opearator in Js 2

--- Day - 009/operator.js
+++ Day - 009/operator.js
@@ -23,6 +23,13 @@ console.log(!"");
 console.log(!"Eyahiya");
 console.log(!!"Eyahiya");
 
-// 
+console.log(true && true);
+console.log(true && false);
+console.log(false && false);
+console.log(false || false);
+console.log(true || false);
+console.log(true || true);
+
+
 
 
