3c0ed96f8f0a653d9d8e0e1af7c4aac11625a3dd
eyahiya-dev <eyahiya.dev@gmail.com>
2023-02-09 21:10:12 +0600

    Practice Operator in JS

---
+++ Day - 007/operator.js
@@ -0,0 +1,19 @@
+// practice Operator
+
+// Arithmatic Operator
+const value1 = 6;
+const value2 = 2;
+
+console.log(value1 + value2);
+console.log(value1 - value2);
+console.log(value1 * value2);
+console.log(value1 / value2);
+console.log(value1 % value2);
+console.log(value1 ** value2); //also knoow as superscript. All we knew it mulitplication with power.
+
+// Increment and Decrement
+
+let num1 = 6;
+num1 = num1 + 5
+
+console.log(num1);
