5f6a5044d79826ec5399b61694f6914cf9aa2bdf
eyahiya-dev <eyahiya.dev@gmail.com>
2023-02-18 16:40:57 +0600

    Practice Conditionals in Js

---
+++ Day - 010/conditionals.js
@@ -0,0 +1,42 @@
+// If else  Conditional
+const studyLevel = "average";
+
+if (studyLevel === "hard") {
+  console.log("You will pass");
+} else if (studyLevel === "average") {
+  console.log("You will make an average result");
+} else {
+  console.log("You will fail");
+}
+
+const age = 18;
+
+// if (age) {
+//     console.log('You can vote');
+// } else {
+//     console.log(`Your're not aligible for Vote`);
+// }
+if (age === 18) {
+  console.log("You can vote");
+} else {
+  console.log(`Your're not aligible for Vote`);
+}
+
+// SwichCase Conditional
+
+const var1 = "average";
+
+switch (var1) {
+  case "hard":
+    console.log("Your will pass");
+    break;
+  case "average":
+    console.log("result average");
+    break;
+  default:
+    console.log("You must study");
+}
+
+
+// Ternary operator
+
