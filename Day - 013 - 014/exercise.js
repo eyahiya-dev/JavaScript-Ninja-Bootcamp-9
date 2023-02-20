0b0a56fcab0c9f748d864e0ab7c67917c6ab7adb
eyahiya-dev <eyahiya.dev@gmail.com>
2023-02-20 14:37:08 +0600

    JS

---
+++ Day - 013 - 014/exercise.js
@@ -0,0 +1,26 @@
+/**
+ * Write a function that takes two strings (a and b) as arguments ---------- (3)
+ * If a contains b, append b to the beginning of a
+ * If not, append it to the end
+ * Return the concatenation
+ * 
+ * function myFunction(a, b) {
+    // your code
+ * }
+
+    myFunction('cheese', 'cake') //'cheesecake'
+    myFunction('lips', 's') //'slips'
+    myFunction('Java', 'script') // 'Javascript'
+    myFunction(' think, therefore I am', 'I') // 'I think, therefore I am'
+ */
+
+function myFunction(a, b) {
+    return a+b
+}
+console.log(myFunction("cheese", "cake"));
+console.log(myFunction("lips", "s"));
+console.log(myFunction("Java", "script"));
+console.log(myFunction(" think, therefore I am", "I"));
+
+
+// i paused this section for later.
